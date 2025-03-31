6417d4a02214d27f4f07fb8bb8e3bc83037bcf80
vophuhao <vophuhao2004@gmail.com>
2025-03-31 15:29:03 +0700

    thanh toan qr

---
+++ app/src/main/java/com/netflixcloneui/model/VNPayResponse.java
@@ -0,0 +1,31 @@
+package com.netflixcloneui.model;
+
+public class VNPayResponse {
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    String status;
+    String message;
+    String amount;
+}
